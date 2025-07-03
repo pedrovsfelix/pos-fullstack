4ab9edda40bad76733aa2f9d0a39fdbe7fbfd8be
pedrovsfelix <pedrovitorsf@gmail.com>
2025-07-03 14:35:44 -0300

    :sparkles: feat: Custom Types Extends Interface

--- 00-FUNDAMENTOS-TYPESCRIPT/custom.types.ts
+++ 00-FUNDAMENTOS-TYPESCRIPT/custom.types.ts
@@ -9,3 +9,24 @@ function newProduct(product: IProduct ) {
 }
 
 newProduct({ id: 1, name: "Produto A" });
+
+// Interfaces extendida
+interface IPerson {
+    id: number,
+    name: string,
+}
+
+interface ITeacher extends IPerson {
+    // id: number,
+    // name: string,
+    subjects: string[],
+}
+
+interface IStudent extends IPerson {
+    // id: number,
+    // name: string,
+    age: number,
+}
+
+let teacher: ITeacher = { id: 1, name: "Pedro", subjects: ["Math", "Science"] };
+let student: IStudent = { id: 2, name: "Babu", age: 5 };
