e0d8842ebc5fb2f3207d9f96c15a9cfad64b7467
pedrovsfelix <pedrovitorsf@gmail.com>
2025-07-04 16:50:28 -0300

    :sparkles: feat: Diferenças entre Type e Interface

--- 00-FUNDAMENTOS-TYPESCRIPT/custom.types.ts
+++ 00-FUNDAMENTOS-TYPESCRIPT/custom.types.ts
@@ -64,3 +64,41 @@ type Teacher = Person & {
 type Student = Person & {
     age: number,
 }
+
+// Diferença entre Type e Interface
+// Type é mais flexível, permite intersecção e união de tipos, enquanto Interface é mais adequada para definir contratos e herança. Interfaces podem ser estendidas, enquanto Types podem ser combinados.
+// Exemplo de uso de Type e Interface
+
+interface IBaseProduct {
+    price: number,
+}
+
+interface IProduct extends IBaseProduct {
+    id: number,
+    name: string
+}
+
+// Interfaces é permitido criar múltiplas declarações com o mesmo nome
+interface IProduct {
+    quantity: number,
+}
+
+// Type não permite isso, se tentar declarar duas vezes dará erro de duplicação
+type TBaseProduct = {
+    price: number,
+}
+
+type TProducts = TBaseProduct & {
+    id: number,
+    name: string
+}
+
+let product1: IProduct = { id: 1, name: "Produto A", price: 100, quantity: 10 };
+let product2: TProducts = { id: 2, name: "Produto B", price: 200 };
+
+// Exemplo de tipos primitivos
+type TypeString = string;
+type TypeNumber = number;
+
+interface X extends string {
+    // Não é possível estender tipos primitivos com interface}
