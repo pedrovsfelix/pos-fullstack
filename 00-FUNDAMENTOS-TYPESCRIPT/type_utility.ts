2ef898e4cbd77b782a53d27729ff075eb1879f51
pedrovsfelix <pedrovitorsf@gmail.com>
2025-07-07 21:20:45 -0300

    :sparkles: feat: Utilitario Pick

--- 00-FUNDAMENTOS-TYPESCRIPT/type_utility.ts
+++ 00-FUNDAMENTOS-TYPESCRIPT/type_utility.ts
@@ -11,3 +11,13 @@ const newUser: User = { id: 1, name: 'Pedro', email: 'pedro@email.com' }
 
 // Tornando apenas as propriedades que quero atualizar, deixando os campos opcionais ao invés de apenas o E-mail.
 const updatedUser: Partial<User> = { name: 'Pedro Felix' }
+
+// Pick
+interface Book {
+    title: string;
+    pages: number;
+    author: string;
+}
+
+// Utilizamos este utilitário para escolher a propriedade que desejamos reaproveitar e editar.
+const book: Pick<Book, "title"> = { title: "Typescript" }
