c1fcb9f3acf9768d5f89839b7e68e0e875681288
pedrovsfelix <pedrovitorsf@gmail.com>
2025-07-10 16:09:56 -0300

    :sparkles: feat: HTTP Status code

--- 01-FUNDAMENTOS-NODEJS/src/server.js
+++ 01-FUNDAMENTOS-NODEJS/src/server.js
@@ -17,6 +17,13 @@ import http from 'http';
 // PATCH -> Atualizar uma informação única ou específica de um recurso do back-end
 // DELETE -> Remover uma informação do back-end
 
+// HTTP response status code
+// status informativos (100-199)
+// status de sucesso (200-299)
+// status de redirecionamento (300-399)
+// status de erro do cliente (400-499)
+// status de erro do servidor (500-599)
+
 // Stateful -> Aplicações com armazenamento local, perdem os dados após reiniciar o servidor
 // Stateless -> Aplicações sem armazenamento local, não perdem os dados após reiniciar o servidor (ex: REST APIs, MongoDB)
 
@@ -40,11 +47,11 @@ const server = http.createServer((req, res) => {
             email: 'babu@email.com'
         })
         // Criar usuários
-        return res.end('Criação de usuários');
+        return res.writeHead(201).end();
 
     }
 
-    return res.end('Hello World');
+    return res.writeHead(404).end();
 })
 
 server.listen(3333)
